9f8ed20f1ba81d5eb3d950aa1dacdb2c1994e7b5
Jomar <jomarcalandria@gmail.com>
2024-08-28 10:13:11 +0800

    Initial Commit

---
+++ CodingExamSevenSeven/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CodingExamSevenSeven
+//
+//  Created by Jomar Calandria on 8/28/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
